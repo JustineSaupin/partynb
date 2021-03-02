615eb199be7189767af55292195c2dbc01657cfe
Justine Saupin <sau.justine@gmail.com>
2021-03-02 11:44:54 +0100

    definition des routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,29 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # en tant que renter
   resources :party_places, only: [:index, :show] do
     resources :reservations, only: [ :create]
   end
 
+  resources :reservations, only: [] do
+    member do
+      patch :cancel
+    end
+  end
+
+  # en tant que owner et ou renter
+  resource :profile, only: [:show]
+
+  # en tant que owner
+  namespace :owner do
+    resources :party_places, only: [:new, :create]
+    resources :reservations, only: [] do
+      member do
+        patch :accept
+        patch :decline
+      end
+    end
+  end
 end
