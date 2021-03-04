238795a0f56ed80a97cc5b88ac2c4d8304e23f25
Geoffrey <mesniergeoffrey@gmail.com>
2021-03-04 20:34:51 +0100

    Search bar

--- app/controllers/party_places_controller.rb
+++ app/controllers/party_places_controller.rb
@@ -5,7 +5,15 @@ class PartyPlacesController < ApplicationController
   end
 
   def index
-    @party_places = PartyPlace.all
+    if params[:query].present?
+      sql_query = <<~SQL
+        party_places.name @@ :query
+        OR party_places.address @@ :query
+        OR party_places.description @@ :query
+      SQL
+      @party_places = PartyPlace.where(sql_query, query: "%#{params[:query]}%")
+    else
+      @party_places = PartyPlace.all
+    end
   end
-
 end
