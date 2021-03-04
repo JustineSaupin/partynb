5277b1df447e7305996ff726012df5e72036ad66
Justine Saupin <sau.justine@gmail.com>
2021-03-04 15:49:11 +0100

    controller partyplaces

--- app/controllers/party_places_controller.rb
+++ app/controllers/party_places_controller.rb
@@ -13,6 +13,14 @@ class PartyPlacesController < ApplicationController
   end
 
   def create
+    @party_place = PartyPlace.new(pp_params)
+    @party_place.save
+    redirect_to profile_path
+  end
 
+  private
+
+  def pp_params
+    params.require(:party_place).permit(:name, :address, :description, :price_per_day)
   end
 end
